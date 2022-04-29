4c08ffe300305230028192820d4bc54b9cb72166
nasirdiu <nasir.joypurhat@gmail.com>
2022-04-29 23:15:36 +0600

    footer

--- src/components/Share/Footer/Footer.js
+++ src/components/Share/Footer/Footer.js
@@ -1,12 +1,93 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
-      <h1 className="text-center text-primary mt-5">Footer</h1>
-      
-    </div>
-    
+    <footer className="bg-dark text-white pt-5 pb-4">
+      <div className="container text-center text-md-left">
+        <div className="row text-center text-md-left">
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Electronics Warehouse
+            </h5>
+            <p>
+              Evaluating the advantages of a warehouse management system is one
+              of the many critical aspects of growing businesses. A warehouse
+              management system will significantly help the growing companies
+              possessing on-hand stocks and inventories.
+            </p>
+          </div>
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Products
+            </h5>
+            <p>
+              <a href="#" className="text-white text-decoration-none">
+                The Providers
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white text-decoration-none">
+                The Providers
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white text-decoration-none">
+                The Providers
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white text-decoration-none">
+                The Providers
+              </a>
+            </p>
+          </div>
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Usefull Link
+            </h5>
+            <p>
+              <a href="#" className="text-white text-decoration-none">
+                The Providers
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white text-decoration-none">
+                The Providers
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white text-decoration-none">
+                The Providers
+              </a>
+            </p>
+            <p>
+              <a href="#" className="text-white text-decoration-none">
+                The Providers
+              </a>
+            </p>
+          </div>
+          <div className="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h5 className="text-uppercase mb-4 font-weight-bold text-warning">
+              Social Media
+            </h5>
+            <p>
+              <i className="fas fa-home mr-3"></i>New Yourk,Ny 3 ,us
+            </p>
+          </div>
+        </div>
+        <div className="row align-item-center">
+          <div className="col-md-7 col-lg-8">
+            <p>
+              Copyright 2022 All right reserved by:
+              <a href="#" className="text-warning text-decoration-none">
+                <strong> Providers</strong>
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
